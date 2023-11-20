aa13b78826318fc673e07a4e2906782e2c31e5a8
Elias Ablan <eliasablan93@gmail.com>
2023-11-20 14:23:53 -0400

    branding update

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <Image
-      height={130}
-      width={130}
-      alt="logo"
-      src="/logo.svg"
-    />
-  )
-}
+    <p className="flex justify-start align-center gap-x-2">
+      <Image height={30} width={30} alt="logo" src="/icon_logo.png" />
+      <span className="mt-0.5 font-medium text-indigo-900 text-xl">
+        Mango LMS
+      </span>
+    </p>
+  );
+};
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: "Mango LMS - Online courses",
+  description: "Get your Development and Design courses here",
+};
